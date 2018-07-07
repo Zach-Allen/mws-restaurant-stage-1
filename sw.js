52dd14b6cb347700bf2356f0d686d3a62f97b063
Zach-Allen <zach.allen2814@gmail.com>
2018-07-06 23:25:41 -0600

    Improved Offline caching logic

--- sw.js
+++ sw.js
@@ -1,37 +1,27 @@
 var myCache = "restaurant-reviews-cache-v1";
-var urlsToCache = [
-  "/",
-  "js/sw-install.js",
-  "js/dbhelper.js",
-  "js/main.js",
-  "js/restaurant_info.js",
-  "data/restaurants.json",
-  "img/1.jpg",
-  "img/2.jpg",
-  "img/3.jpg",
-  "img/4.jpg",
-  "img/5.jpg",
-  "img/6.jpg",
-  "img/7.jpg",
-  "img/8.jpg",
-  "img/9.jpg",
-  "img/10.jpg",
-  "css/styles.css"
-];
-
+// var urlsToCache = [
+//   "/",
+//   "js/sw-install.js",
+//   "js/dbhelper.js",
+//   "js/main.js",
+//   "js/restaurant_info.js",
+//   "data/restaurants.json",
+//   "css/styles.css"
+// ];
+//
 self.addEventListener("install", function(event) {
-  console.log("install");
+  console.log("SW Installed");
   // Perform install steps
   event.waitUntil(
     caches
       .open(myCache)
       .then(function(cache) {
-        console.log("Opened cache");
-        return cache.addAll(urlsToCache);
+        console.log("Cache Opened");
+        // return cache.addAll(urlsToCache);
       })
-      .then(function(res) {
-        console.log("Urls added to cache");
-      })
+      // .then(function(res) {
+      //   console.log("Urls added to cache");
+      // })
       .catch(function(...err) {
         console.error(err);
       })
